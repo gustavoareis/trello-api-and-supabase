d1f27ebd07ed6228abff7a74b5033442f36b2659
Gustavo Almeida <gustavoalreisz@gmail.com>
2025-09-22 09:34:25 -0300

    Delete credenciais.py

--- credenciais.py
+++
@@ -1,9 +0,0 @@
-trello_credentials = {
-    "api_key": "b055a9735d355c0fd472e20679af671d",
-    "token": "ATTAe6e684b2b26fc0d6146af3b69edd29ac6a0f8f33c225ca4d337f8a7f306a71c0DD1F5BF7",
-}
-
-supabase_credentials = {
-    "url": "https://api-supabase.ocenergy.com.br",
-    "key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaXNzIjoic3VwYWJhc2UiLCJzdWIiOiJzZXJ2aWNlX3JvbGUiLCJleHAiOjIwNzA5MDUwNzF9.ntm52yom-3uF2de_H-mohuyMEv21JnO3QwDiZB0Gc68",
-}
